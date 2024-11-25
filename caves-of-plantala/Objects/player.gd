dd6e7cc4f62db711d468558ede3dc4636dff1722
protogenposting <86360309+protogenposting@users.noreply.github.com>
2024-11-25 16:26:59 -0500

    adjustments

--- caves-of-plantala/Objects/player.gd
+++ caves-of-plantala/Objects/player.gd
@@ -2,10 +2,11 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -500.0
+const JUMP_VELOCITY = -380.0
 const ACCELERATION = 50
 const MAX_COYOTE_TIME = 5
 const JUMP_WINDOW = 7
+const MAX_FALL_SPEED = 1000
 
 var coyoteTime = 0
 
@@ -15,6 +16,8 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		if velocity.y > MAX_FALL_SPEED:
+			velocity.y = MAX_FALL_SPEED
 	
 	coyoteTime -= delta * 60
 	
